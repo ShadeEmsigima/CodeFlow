07237c337defae2caa27b8639f9e8f7b77d24927
ShadeEmsigima <thisisforlis@duck.com>
2023-01-31 10:40:03 +0100

    modifying primitives and objects in enhanced for loops

--- CodeFlow.java
+++ CodeFlow.java
@@ -1,10 +1,11 @@
-import javax.management.Notification;
-
 public class CodeFlow{
     public static void main(String[] args) {
     {
         System.out.println("===============================\n");
-    }       
+    }   
+    
+    // Switch statements 1
+   {
     int cookiesInStash = 20;
     final int maxCookies = 100; // mark final and initialize value to have switch label be accepted
 
@@ -19,12 +20,12 @@ public class CodeFlow{
       case 10*2 : System.out.println("I see " + 10*2 + " cookies.");
       default : System.out.println("Could not have a peek inside cookie stash Dx");
     }
-    
-
-{
-    System.out.println();
 }
 
+    Enter();
+
+    // Switch statements 2
+    {
     String dayOfWeek = "Monday"; // it is worth mentioning that, while int may be declared without a value, the methods like Switch WILL take offense to that.
 
     // consider the switch statement like a restless worker that wants to know exactly what to do;
@@ -43,23 +44,74 @@ public class CodeFlow{
         case "Friday" : System.out.println("Final day of the week, prepare for the next.");
         default : System.out.println("This should be the weekend.");
     }
+}
 
+    Enter();
+
+    // For loop
     {
-        System.out.println("\n===============================");
-    }
-
     int i1 = 1;
     short s1 = 2;
     for (int beep = 6 ; s1> 0 ; i1++, s1--, m1()){
         System.out.println("Looping the for loop");// code block must include break to get to rest of code
     }
+}
 
-    {
-        System.out.println("\n===============================");
+   Enter();
+    
+    int dogs = 0;
+    StringBuilder sb = new StringBuilder(" intense chocolate cake");
+    int someNums[] = {2,10,5};
+
+    System.out.println("Starting with " + dogs + " dogs.");
+    for (int num : someNums){
+        dogs += num;
+        System.out.println(dogs);
+        sb.insert(0, " extra");
     }
+    System.out.println("Ended up with " + dogs + " dogs and" + sb + "!");
+
+    System.out.println();
+
+    StringBuilder pets[] = {
+        new StringBuilder("Bunny"),
+        new StringBuilder("Fish"),
+        new StringBuilder("Kat")
+    };
+
+    for (StringBuilder x : pets)
+    System.out.println(x);
+    for (StringBuilder x : pets){
+        x.append(" is happy");
     }
+    for (StringBuilder x : pets)
+    System.out.println(x);
+
+
+    System.out.println();
+    StringBuilder mounts[] = {  //creatre an array of objects
+        new StringBuilder("Legendary dragon"),
+        new StringBuilder("Sandy ostrich"),
+        new StringBuilder("Dream whale")
+    };
+
+    for (StringBuilder x : mounts)  // print out their initial value
+        System.out.println(x);
+    
+    for (StringBuilder x : mounts){
+    x = new StringBuilder("Default horse"); // change the reference
+    //System.out.println(x);            // notice how this would have printed horse, as X is but a local variable
+    }
+
+    for (StringBuilder x : mounts)
+    System.out.println(x);
+
+    Enter();
+}
 
     static void m1 (){
         System.out.println("Executed m1");
     }
+
+    static void Enter(){System.out.println("\n========================="+"\n");}
 }
